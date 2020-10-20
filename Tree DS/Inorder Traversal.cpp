cf73b1020ce861fa4a3766f3ea4a5ece902ec652
pranjal <pranjalpm9521gmail.com>
2020-10-20 13:10:33 +0530

    inorder traversal

---
+++ Tree DS/Inorder Traversal.cpp
@@ -0,0 +1,45 @@
+//method 1: recursive method
+
+class Solution {
+public:
+    
+    TreeNode* treeTraversal(TreeNode* root,vector<int> &res){
+        if(root==NULL) return NULL;
+        
+        treeTraversal(root->left,res); 
+        res.push_back(root->val);
+        treeTraversal(root->right,res); 
+        
+        return root;
+    }
+    
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        TreeNode* head = treeTraversal(root,res);
+        return res;
+    }
+};
+
+//method 2: using stack
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> v;
+        if(root==NULL)return v;
+        stack<TreeNode*> s;
+        TreeNode* curr = root;
+        
+        while(!s.empty() || curr!=NULL){
+            while(curr!=NULL){
+                s.push(curr);
+                curr = curr->left;
+            }
+            curr = s.top();
+            s.pop();
+            v.push_back(curr->val);
+            curr=curr->right;
+        }
+        return v;
+    }
+};
